7a69593e1c1df440063858d5369defe97f8b66c6
Daniel <henio09@wp.pl>
2022-12-21 18:14:21 +0100

    git commit -m"dadas"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import './App.css';
 import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+
+  const [search,setSearch] = useState('Naruto');
+  const [animeData,setAnimeData]=useState();
+
   const getData = async() => {
     const res = await fetch("https://api.jikan.moe/v4/anime?q=naruto&sfw");
     const resData = await res.json();
-    console.log(resData);
+    setAnimeData(resData.data);
   }
 
   useEffect(()=>{
@@ -14,8 +20,11 @@ function App() {
     
   return (
     <div className="App">
-      <h3>fsdfsdf</h3>
-      <div>{getData}</div>
+      <h3>MyAnimeList</h3>
+      <div className='search-box'>
+        <input type="search" placeholder="Type anime name here" 
+        onChange={(e)=>setSearch(e.target.value)}/>
+      </div>
     </div>
   );
 }
