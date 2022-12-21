4ded482c5ff510d677f679bff650f2ed7ca5616c
Daniel <henio09@wp.pl>
2022-12-21 19:05:16 +0100

    git commit -m"dadsa"

---
+++ src/AnimeList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const AnimeList = ({animelist}) => {
+    return (
+        <>
+        {
+            animelist ?(
+                animelist.map((anime,index)=>{
+                    return(
+                <div className="card">
+                    <img src={anime.images.webp.image_url}/>
+                    <div className="anime-info">
+                    <h4>{anime.title}</h4>
+                    </div>
+                </div>
+                    )
+                })
+            ):"Not Found"
+        }   
+        </>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { AnimeList } from './AnimeList';
 
 function App() {
 
@@ -8,24 +9,35 @@ function App() {
   const [animeData,setAnimeData]=useState();
 
   const getData = async() => {
-    const res = await fetch("https://api.jikan.moe/v4/anime?q=naruto&sfw");
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&sfw`);
     const resData = await res.json();
     setAnimeData(resData.data);
+    console.log(resData.data);
   }
 
   useEffect(()=>{
     getData()
-  },[])
+  },[search])
    
     
   return (
     <div className="App">
-      <h3>MyAnimeList</h3>
-      <div className='search-box'>
-        <input type="search" placeholder="Type anime name here" 
-        onChange={(e)=>setSearch(e.target.value)}/>
+      <header>
+        <h3>MyAnimeList</h3>
+        <div className='search-box'>
+          <input type="search" placeholder="Type anime name here" 
+          onChange={(e)=>setSearch(e.target.value)}/>
+        </div>
+      </header>
+
+      <div className='anime-row'>
+        <h2 className='text-heading'>Anime</h2>
+        <div className='row'>
+          <AnimeList animelist={animeData}/>
+        </div>
       </div>
-    </div>
+
+    </div>   
   );
 }
 
