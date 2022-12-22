23acc8e4f1712646c59de356df6b45e5756779ea
Daniel <henio09@wp.pl>
2022-12-23 00:28:23 +0100

    fsdfsd

--- src/App.js
+++ src/App.js
@@ -1,72 +1,72 @@
 import './App.css';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AnimeList } from './AnimeList';
 import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [animeData, setAnimeData] = useState([]);
 
-  const [search,setSearch] = useState('');
-  const [animeData,setAnimeData]=useState();
+  const getData = async () => {
+    try {
+      const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&sfw`);
+      const resData = await res.json();
+      setAnimeData(resData.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  const getData = async() => {
-    const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&sfw`);
-    const resData = await res.json();
-    setAnimeData(resData.data);
-  }
+  useEffect(() => {
+    getData();
+  }, [search]);
 
-  useEffect(()=>{
-    getData()
-  },[search]);
-
-  const handleOnSearch = async (string, results) => {
-    // onSearch will have as the first callback parameter
-    // the string searched and for the second the results.
-    
-    await setSearch(string);
-  }
+  const handleOnSearch = (string, results) => {
+     setSearch(string);
+  };
 
   const handleOnHover = (result) => {
-    // the item hovered
-    console.log(result)
-  }
+    console.log(result);
+  };
 
   const handleOnSelect = (item) => {
-    // the item selected
-    console.log(item)
-  }
+    console.log(item);
+  };
 
   const handleOnFocus = () => {
-    console.log('Focused')
-  }
+    console.log('Focused');
+  };
 
   const formatResult = (item) => {
     return (
       <>
-        <span key={item.name} style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
+        <span key={item.name} style={{ display: 'block', textAlign: 'left' }}>
+          {item.name}
+        </span>
       </>
-    )
-  }
+    );
+  };
 
-  const items = animeData ? animeData.map((anime,index) => ({name: anime.title, id: index})) : [];
+  const items = animeData ? animeData.map((anime, index) => ({ name: anime.title, id: index })) : [];
 
   console.log(animeData);
   return (
     <div className="App">
       <header>
         <h3>MyAnimeList</h3>
-        <div className='search-box'>
-           <ReactSearchAutocomplete
+        <div className="search-box">
+          <ReactSearchAutocomplete
             items={items}
             onSearch={handleOnSearch}
             onHover={handleOnHover}
             onSelect={handleOnSelect}
             onFocus={handleOnFocus}
             autoFocus
-            placeholder = "Type anime title"
+            placeholder="Type anime title"
             formatResult={formatResult}
           />
         </div>
+
       </header>
 
       <div className='anime-row'>
